a0256b8613791566a1edb7715f1683eccf0dc79d
citogok <47094667+kenthusblog@users.noreply.github.com>
2022-08-02 00:14:33 +0700

    Update BottomNavigation.jsx

--- components/BottomNavigation.jsx
+++ components/BottomNavigation.jsx
@@ -29,7 +29,16 @@ const navs = [
     title: 'Search',
     path: ['/search'],
     icon: <SearchCircleIcon className="w-6" />
-  }
+  },
+  {
+  return (
+    <footer className="p-3 text-white bg-green-500 text-center">
+      Made with <span className="text-rose-500">❤</span>
+      Kenthus <br/>
+      Jatim - Indonesia
+       </footer>
+  )
+}
 ]
 
 export default function BottomNavigation() {
