5b981a6e40774b822687f7c099aa433867a2d7c1
citogok <47094667+kenthusblog@users.noreply.github.com>
2022-08-01 23:36:31 +0700

    Update Layouts.jsx

--- components/Layouts.jsx
+++ components/Layouts.jsx
@@ -11,18 +11,7 @@ export default function Layouts({ children, title }) {
       <Head>
        <meta name="viewport" content="width=device-width, initial-scale=1" />
        <meta charSet="utf-8" />
-       <meta name="description" content={description}></meta>
-       <meta property="og:title" content={pageTitle} key="ogtitle" />
-       <meta property="og:description" content={description} key="ogdesc" />
-        {/* Twitter */}
-      <meta name="twitter:card" content="summary" key="twcard" />
-      <meta name="twitter:creator" content={twitterHandle} key="twhandle" />
-      {/* Open Graph */}
-       <meta property="og:url" content={currentURL} key="ogurl" />
-       <meta property="og:image" content={previewImage} key="ogimage" />
-       <meta property="og:site_name" content={siteName} key="ogsitename" />
-       <meta property="og:title" content={pageTitle} key="ogtitle" />
-       <meta property="og:description" content={description} key="ogdesc" />
+       
               
         <title>{title ? `${appName} - ${title}` : appName}</title>
         <link rel="icon" href="/favicon.ico" />
