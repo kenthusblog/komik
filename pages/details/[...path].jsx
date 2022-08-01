75da71be11c767e5d19185bfd84bd19ff5d027bf
citogok <47094667+kenthusblog@users.noreply.github.com>
2022-08-02 01:01:14 +0700

    Update [...path].jsx

--- pages/details/[...path].jsx
+++ pages/details/[...path].jsx
@@ -98,7 +98,6 @@ export default function Details() {
           </table>
         </div>
       </div>
-    <div class="sharethis-inline-share-buttons"></div>
       <h2 className="text-xl font-bold text-green-500">Genre</h2>
       <p className="mb-3">
         {genres.map((g, i) => (
