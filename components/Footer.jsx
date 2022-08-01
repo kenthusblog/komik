d711b2a60f7312266d2d8d393c10e78214ccff04
citogok <47094667+kenthusblog@users.noreply.github.com>
2022-08-02 00:29:38 +0700

    Update Footer.jsx

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,8 +1,9 @@
 export default function Footer() {
   return (
     <footer className="p-3 text-white bg-green-500 text-center">
-      Made with <span className="text-rose-500">❤</span>  
-         Kenthus <br/>
+        Made with 
+        <span className="text-rose-500">❤ </span>  
+                          Kenthus <br/>
         Jatim - Indonesia
        </footer>
   )
