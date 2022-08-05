6c2df8edea056a07ed8525845f961d605b5db2bf
citogok <47094667+kenthusblog@users.noreply.github.com>
2022-08-05 21:42:26 +0700

    Update [...path].jsx

--- pages/details/[...path].jsx
+++ pages/details/[...path].jsx
@@ -59,7 +59,14 @@ export default function Details() {
         </div>
         <div><script type="module">
   import shareButtons from 'https://cdn.skypack.dev/share-buttons';
-</script>
+</script><div class="share-btn">
+    <a class="btn-vk" data-id="vk">VK</a>
+    <a class="btn-facebook" data-id="fb">Facebook</a>
+    <a class="btn-twitter" data-id="tw">Twitter</a>
+    <a class="btn-telegram" data-id="tg">Telegram</a>
+    <a class="btn-mail" data-id="mail">EMail</a>
+</div>
+
           <table className="w-full sticky top-3">
             <tr>
               <td>Jenis Komik</td>
