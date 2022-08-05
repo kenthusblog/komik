f4ef545d62f6da568d8fe0328b0e8717a41d4ccf
citogok <47094667+kenthusblog@users.noreply.github.com>
2022-08-06 02:18:23 +0700

    Update Footer.jsx

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -3,12 +3,12 @@ export default function Footer() {
     <footer className="p-3 text-white bg-green-500 text-center">
       Made with <span className="text-rose-500">❤</span> by{' '}
       <a
-        href="https://github.com/feri-irawan"
+        href="/"
         target="_blank"
         rel="noopener noreferrer"
         className="hover:underline"
       >
-        Feri Irawan
+        KTS
       </a>
     </footer>
   )
