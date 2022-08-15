c6242f18c930a58a22441d04be8c17c45b726a31
citogok <47094667+kenthusblog@users.noreply.github.com>
2022-08-15 18:56:53 +0700

    Update Header.jsx

--- components/Header.jsx
+++ components/Header.jsx
@@ -3,9 +3,8 @@ import Image from 'next/image'
 
 export default function Header() {
   return (
-    <div className="p-3 flex justify-between items-center bg-green-500 text-white">
-  <Image src="/baca-logo.png" width={20} height={20} />
-      <div className="">
+    <div className="p-3 flex justify-between items-center bg-green-500 text-white"  class="bg-gradient-to-r from-green-400 to-blue-500 hover:from-pink-500 hover:to-yellow-500">
+       <div className="">
         <Link href="/">
           <a className="text-xl font-bold">BacaKomik</a>
         </Link>
