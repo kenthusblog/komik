60127565962f28e423cf0171c49ae6591df3c54b
citogok <47094667+kenthusblog@users.noreply.github.com>
2022-08-15 19:11:00 +0700

    Update Footer.jsx

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className="p-3 text-white bg-green-500 text-center">
+    <footer className="p-3 text-white text-center">
     <div class="bg-gradient-to-r from-green-400 to-blue-500 hover:from-pink-500 hover:to-yellow-500 ...">
       Made with <span className="text-rose-500">❤</span> by{' '}
       <a
