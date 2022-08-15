a7c27ec31ae67bbcd117ac108bbbd940b7b47b3e
citogok <47094667+kenthusblog@users.noreply.github.com>
2022-08-15 18:11:56 +0700

    Update Header.jsx

--- components/Header.jsx
+++ components/Header.jsx
@@ -4,7 +4,6 @@ import Image from 'next/image'
 export default function Header() {
   return (
     <div className="p-3 flex justify-between items-center bg-green-500 text-white">
-        <img class="w-16 md:w-32 lg:w-48" src="baca-logo.png">
 
       <div className="">
         <Link href="/">
