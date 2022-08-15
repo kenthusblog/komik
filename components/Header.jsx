56cf701857b2c01092ddba5511d4fed8670596fd
citogok <47094667+kenthusblog@users.noreply.github.com>
2022-08-15 17:44:06 +0700

    Update Header.jsx

--- components/Header.jsx
+++ components/Header.jsx
@@ -6,7 +6,8 @@ export default function Header() {
      <div className="p-3 flex justify-between items-center bg-green-500 text-white">
       <div className="">
          <Link href="/">
-          <a className="text-xl"> <Image src="/baca-logo.png" width={25} height={25} />
+          <a className="text-xl"> <Image src="/baca-logo.png" width={25} height={25} /><div>
+          </div>
          BacaKomik</a>
         </Link>
        </div>
