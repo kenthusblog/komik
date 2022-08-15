fa71fb0f65a24505d9866e13dffb2d4b92dcdedc
citogok <47094667+kenthusblog@users.noreply.github.com>
2022-08-15 17:12:31 +0700

    Update Header.jsx

--- components/Header.jsx
+++ components/Header.jsx
@@ -6,8 +6,17 @@ export default function Header() {
      <div className="p-3 flex justify-between items-center bg-green-500 text-white">
      
     <div className="">
-            <div><Image src="/baca-logo.png" width={30} height={30}  /></div>
+            <Image src="/baca-logo.png" width={35} height={35}  />
         <Link href="/">
+          <img
+            src="/baca-logo.png"
+            alt={title}
+            className="w-full"
+            style={{ minHeight: '150px' }}
+            onError={() =>
+              setSrc('https://dummyimage.com/600x400/86efad/22c55e')
+            }
+          />
           <a className="text-xl font-bold">BacaKomik</a>
         </Link>
        </div>
