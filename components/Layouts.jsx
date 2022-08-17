58a6e17be2917df9528b249a1638b1fe6e45e0fe
citogok <47094667+kenthusblog@users.noreply.github.com>
2022-08-17 21:21:40 +0700

    Update Layouts.jsx

--- components/Layouts.jsx
+++ components/Layouts.jsx
@@ -11,18 +11,54 @@ export default function Layouts({ children, title }) {
       <Head>
         <title>{title ? `${appName} - ${title}` : appName}</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        
-         <meta name="keywords" content="komik online, baca komik" />
-        
-         <meta property="og:image" content="/baca-komik.jpg"/>
+        <meta name="description" content={description} />
+      <meta key="og_type" property="og:type" content={ogType} />
+      <meta key="og_title" property="og:title" content={title} />
+      <meta key="og_description" property="og:description" content={description} />
+      <meta key="og_locale" property="og:locale" content="en_IE" />
+      <meta key="og_site_name" property="og:site_name" content={siteName} />
+      <meta key="og_url" property="og:url" content={canonical ?? DOMAIN} />
+      <meta key="og_site_name" property="og:site_name" content={siteName} />
+      <meta
+        key="og_image"
+        property="og:image"
+        content={ogImage ?? DEFAULT_OG_IMAGE}
+      />
+      <meta
+        key="og_image:alt"
+        property="og:image:alt"
+        content={`${title} | ${siteName}`}
+      />
+      <meta key="og_image:width" property="og:image:width" content="1200" />
+      <meta key="og_image:height" property="og:image:height" content="630" />
 
-         <meta property="og:title" content="Baca Komik Online Terlengkap"/>
+      <meta name="robots" content="index,follow" />
 
-         <meta property="og:description" content="Baca Komik Online Terlengkap dan Terupdate"/>
-
-         <meta property="og:image:width" content="1200"/>
-
-         <meta property="og:image:height" content="630"/>
+      <meta
+        key="twitter:card"
+        name="twitter:card"
+        content="summary_large_image"
+      />
+      <meta 
+        key="twitter:site" 
+        name="twitter:site" 
+        content={twitterHandle} 
+      />
+      <meta
+        key="twitter:creator"
+        name="twitter:creator"
+        content={twitterHandle}
+      />
+      <meta 
+        key="twitter:title" 
+        property="twitter:title" 
+        content={title} 
+      />
+      <meta
+        key="twitter:description"
+        property="twitter:description"
+        content={description}
+      />
 
      
         <link rel="icon" href="/favicon.ico" />
