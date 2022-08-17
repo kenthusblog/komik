c149b310aa7dd4c14fe22c7d1cb06c6855ebf865
citogok <47094667+kenthusblog@users.noreply.github.com>
2022-08-17 22:15:54 +0700

    Update Errors.jsx

--- components/Errors.jsx
+++ components/Errors.jsx
@@ -3,7 +3,7 @@ import Loading from './Loading'
 
 export function NoData({ error }) {
   return (
-    <Layouts title={title ? `${appName} - ${title}` : appName}>
+    <Layouts title={title}>
       {!error && <Loading message="Sedang memuat komik..." loading={true} />}
       {error && (
         <>
