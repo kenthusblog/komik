91de78eac716095c480914566f86bb226743c334
citogok <47094667+kenthusblog@users.noreply.github.com>
2022-08-17 21:19:31 +0700

    Update Layouts.jsx

--- components/Layouts.jsx
+++ components/Layouts.jsx
@@ -11,7 +11,9 @@ export default function Layouts({ children, title }) {
       <Head>
         <title>{title ? `${appName} - ${title}` : appName}</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        <meta name="keywords" content="komik online, baca komik" />
+        
+         <meta name="keywords" content="komik online, baca komik" />
+        
          <meta property="og:image" content="/baca-komik.jpg"/>
 
          <meta property="og:title" content="Baca Komik Online Terlengkap"/>
