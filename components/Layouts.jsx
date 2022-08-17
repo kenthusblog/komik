3f2138772b331f6fc95bfaa8f12a5151e0557ee7
citogok <47094667+kenthusblog@users.noreply.github.com>
2022-08-17 21:15:02 +0700

    Update Layouts.jsx

--- components/Layouts.jsx
+++ components/Layouts.jsx
@@ -4,8 +4,7 @@ import Contents from './Contents'
 import Footer from './Footer'
 import Header from './Header'
 
-export default function Layouts({ children, title }) 
-{
+export default function Layouts({ children, title }) {
   const appName = 'BacaKomik'
   
   return (
