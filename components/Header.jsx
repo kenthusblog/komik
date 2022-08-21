2d1e4b565e3a880222fda80e55fa22a8b59f6f09
citogok <47094667+kenthusblog@users.noreply.github.com>
2022-08-21 20:53:22 +0700

    Update Header.jsx

--- components/Header.jsx
+++ components/Header.jsx
@@ -4,12 +4,10 @@ export default function Header() {
   return (
    <div class="sticky top-0 text-white">
     <div class="bg-gradient-to-r from-green-400 to-blue-500 hover:from-pink-500 hover:to-yellow-500 ...">
-     <div className="p-3 flex justify-between items-center text-white">
-       <div className="flex">
+       <div className="">
         <Link href="/">
           <a className="text-xl font-bold inline-block duration-300">BacaKomik</a>
         </Link>
-      </div>
     </div>
       <div className=""></div>
     </div>
