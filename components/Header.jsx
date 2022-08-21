fb7d82f5e241893d62837f343e545cf764a31b30
citogok <47094667+kenthusblog@users.noreply.github.com>
2022-08-21 21:03:25 +0700

    Update Header.jsx

--- components/Header.jsx
+++ components/Header.jsx
@@ -2,14 +2,12 @@ import Link from 'next/link'
 
 export default function Header() {
   return (
-   <div class="sticky top-0 text-white">
     <div class="bg-gradient-to-r from-green-400 to-blue-500 hover:from-pink-500 hover:to-yellow-500 ...">
         <div className="p-3 flex justify-between items-center bg-green-500 text-white">
        <div className="">
         <Link href="/">
           <a className="text-xl font-bold inline-block duration-300">BacaKomik</a>
         </Link>
-    </div>
     </div>
 
     </div>
