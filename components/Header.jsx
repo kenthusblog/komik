82c02b3f9f785fc543b4d3023f162927fed1e6dc
citogok <47094667+kenthusblog@users.noreply.github.com>
2022-08-22 18:50:01 +0700

    Update Header.jsx

--- components/Header.jsx
+++ components/Header.jsx
@@ -5,7 +5,6 @@ export default function Header() {
     <div class="bg-gradient-to-r from-green-400 to-blue-500 hover:from-pink-500 hover:to-yellow-500 ...">
         <div className="p-3 flex justify-between items-center text-white">
        <div className="">
-                <img src="/baca-logo.png" />
         <Link href="/">
            <a className="text-xl font-bold inline-block duration-300">
     <span class="before:block before:absolute before:-inset-1 before:-skew-y-3 before:bg-pink-500 relative inline-block">
